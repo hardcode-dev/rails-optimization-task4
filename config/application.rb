c28088fcc851ee0cf7d757922a58beef8d509606
Alexey Pasenko <avpasenko@dasreda.ru>
2020-02-22 16:58:37 +0300

    add skylight

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module PracticalDeveloper
   class Application < Rails::Application
+    config.skylight.environments += ["development"]
     config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.

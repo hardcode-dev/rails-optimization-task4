8b6745ad8d33d6993bf0e33948b499b8ea70bd60
Oleg Sachev <cbi444@gmail.com>
2020-04-12 01:28:35 +0300

    Add metrics path to app loadpath.

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,7 @@ module PracticalDeveloper
     config.autoload_paths += Dir["#{config.root}/app/black_box/"]
     config.autoload_paths += Dir["#{config.root}/app/sanitizers"]
     config.autoload_paths += Dir["#{config.root}/lib/"]
+    config.autoload_paths += Dir["#{config.root}/app/metrics"]
 
     config.active_record.observers = :article_observer, :reaction_observer, :comment_observer
     config.active_job.queue_adapter = :delayed_job

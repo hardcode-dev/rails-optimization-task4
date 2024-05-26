595f540a237d39ca3ec4d5607dd0b469e472e08e
potashin <potashin.nikita@gmail.com>
2024-05-26 20:45:03 +0300

    config: setup local production

--- config/initializers/rack_mini_profiler.rb
+++ config/initializers/rack_mini_profiler.rb
@@ -5,5 +5,8 @@ require "rack-mini-profiler"
 # The initializer was required late, so initialize it manually.
 Rack::MiniProfilerRails.initialize!(Rails.application)
 
-Rails.application.middleware.delete(Rack::MiniProfiler)
-Rails.application.middleware.insert_after(Rack::Deflater, Rack::MiniProfiler)
+if Rails.env.production? || Rails.env.staging?
+  Rails.application.middleware.delete(Rack::MiniProfiler)
+  Rails.application.middleware.insert_after(Rack::Deflater, Rack::MiniProfiler)
+  Rack::MiniProfiler.config.authorization_mode = :allow_authorized
+end
